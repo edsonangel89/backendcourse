16a051e9a3541be4e5332b27efc6381784239e95
Ragnarok software <edsonangelmc@gmail.com>
2023-08-27 20:40:29 -0500

    Added handlebars and socket

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 import express from 'express'
 import prodsR from './routes/products.routes.js'
 import cartsR from './routes/carts.routes.js'
+import __dirname from './utils.js'
+import handlebars from 'express-handlebars'
+import viewsRouter from './routes/views.router.js'
+import {Server} from 'socket.io'
 
 const app = express()
+const httpS = app.listen(8080,() => console.log("Server on port 8080"))
 
+
+app.engine('handlebars',handlebars.engine())
+app.set('views',__dirname + '/views')
+app.set('view engine','handlebars')
+
+app.use(express.static(__dirname + '/public'))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-
+app.use('/',viewsRouter)
 app.use('/api/products',prodsR)
 app.use('/api/carts',cartsR)
 
-app.listen(8080,() => console.log("Server on port 8080"))
+const socketS = new Server(httpS)
+
+socketS.on('connection', (socket) => {
+        console.log('Hello Im a socket')
+    }
+)
+
 
---
+++ src/routes/views.router.js
@@ -0,0 +1,16 @@
+import {Router} from 'express'
+import {ProductManager,Product} from '../ProductManager.js'
+
+const router = Router()
+const manager = new ProductManager()
+
+router.get('/', async (req, res) => {
+    
+    res.render('home', {})
+})
+
+router.get('/realtimeproducts', (req, res) => {
+    res.render('realTimeProducts', {})
+})
+
+export default router
