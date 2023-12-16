140fea40fd056a9f1c537e744c4c9565a28b2ec1
edson angel <edsonangelmc@gmail.com>
2023-12-16 14:04:10 -0700

    Added API documentation

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express'
 import router from './routes/app.routes.js'
 import prodsRouter from './routes/products.routes.js'
 import cartsRouter from './routes/carts.routes.js'
-import __dirname from './utils.js'
+import __dirname from './path.js'
 import handlebars from 'express-handlebars'
 import viewsRouter from './routes/views.routes.js'
 import { Server } from 'socket.io'
@@ -19,6 +19,8 @@ import initialize from './config/passport.js'
 import passport from 'passport'
 import errorHandler from './middleware/errors/index.js'
 import { addInfoLogger } from "./config/logger.js"
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUiExpress from 'swagger-ui-express'
 
 const app = express()
 const httpS = app.listen(8080,() => console.log("Server on port 8080"))
@@ -26,6 +28,20 @@ const httpS = app.listen(8080,() => console.log("Server on port 8080"))
 mongoose.connect(process.env.MONGO_URL)
 .then(async () => {console.log('BDD Conectada')})
 .catch()
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.1',
+        info: {
+            title: 'Documentacion ecommerce',
+            description: 'Documentacion para tienda en linea'
+        }
+    },
+    apis: [`${__dirname}/docs/**/*.yaml`]
+}
+
+const specs = swaggerJsdoc(swaggerOptions)
+app.use('/apidocs',swaggerUiExpress.serve,swaggerUiExpress.setup(specs))
 
 app.engine('handlebars',handlebars.engine())
 app.set('views',__dirname + '/views')
